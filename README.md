b2e32f6edc34194162bf8961c0dee34d4bcaba7a
shseok <gus0674@ajou.ac.kr>
2021-08-13 07:28:14 +0900

    Changed dynamically & Applied localStorage

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
+## Todo List with JS
+
+## 목표
+
+- 자바스크립트로 '할 일'을 기록하는 간단한 project를 만들어보자
+
+## 기능
+
+- 원하는 단어 입력시 바로 아래에 스택형식으로 할 일 기록
+
+- 휴지통 버튼 클릭(삭제)시 생동감을 위해 Animation 추가
+
+- 체크 버튼 클릭(완료)시 하지 않은 일과 구분하기 위해 line으로 표시
+
+- Page Refresh(F5)시 기존 저장된 기록들이 유지
+
+- 선택창에서 3가지(한 일, 할 일, 모든 일)로 분류
+
+## 주의사항
+
+localStorage 사용시 개발자모드에서 Local Storage 내부에 object의 존재유무 확인 후, object가 있다면 모두 주석처리 후, 오직 아래 명령어로만 비워준다.
+
+> <code>localStorage.clear();</code>
+
 ## 도움 받은 사이트 (ref)
+
+font
 
 https://fonts.google.com/specimen/Poppins?query=pop
 
-https://fontawesome.com/v5.15/icons?d=gallery&p=2
-
 https://cdnjs.com/libraries/font-awesome
 
+icon
+
+https://fontawesome.com/v5.15/icons?d=gallery&p=2
+
+after (css)
+
 https://developer.mozilla.org/en-US/docs/Web/CSS/::after
+
+Document.children
+
+https://developer.mozilla.org/en-US/docs/Web/API/Document/children
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // Selectors
-const todoInput = document.querySelector('.todo-input')
-const todoButton = document.querySelector('.todo-button')
-const todoList = document.querySelector('.todo-list')
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listener
+document.addEventListener('DOMContentLoaded', getLocalTodos);
 todoButton.addEventListener('click', addTodo); 
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterToDo);
 
 // Function
 function addTodo(event){
@@ -20,6 +23,8 @@ function addTodo(event){
     newTodo.classList.add('todo-item');
 
     todoDiv.appendChild(newTodo);
+    // ADD TODO TO LOCALSTORAGE
+    saveLocalTodos(todoInput.value);
     //CHECK MARK BUTTON
     const completedButton = document.createElement("button");
     completedButton.innerHTML = '<i class = "fas fa-check"></i>';
@@ -47,6 +52,7 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         // Animation
         todo.classList.add("fall");
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', () => todo.remove());
         // todo.remove();
 
@@ -57,4 +63,90 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.classList.toggle("completed"); // class name change
     }
+}
+
+function filterToDo(e) {
+    const todos = todoList.childNodes;
+    
+    todos.forEach((todo) => {
+        console.log(todo);
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                 if (!todo.classList.contains('completed')) { // contains는 app.js:60의 행위에 대한 반응
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+        
+    });
+}
+
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos")); // 가져와서
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos)); // 다시 세팅
+}
+
+function getLocalTodos() {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos")); // 가져와서
+    }
+    todos.forEach((todo) => { // addTodo 내용에서 수정
+        // Todo DIV
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        // Create LI
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todo;
+        newTodo.classList.add('todo-item');
+
+        todoDiv.appendChild(newTodo);
+        //CHECK MARK BUTTON
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+        completedButton.classList.add('complete-btn');
+        todoDiv.appendChild(completedButton);
+        //CHECK TRASH BUTTON
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+        trashButton.classList.add('trash-btn');
+        todoDiv.appendChild(trashButton);
+        //APPEND TO LIST
+        todoList.appendChild(todoDiv);
+    });
+
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos")); // 가져와서
+    }
+    
+    const todoIndex = todos.indexOf(todo.children[0].innerText);
+    todos.splice(todoIndex, 1);
+    console.log(todos);
+    localStorage.setItem("todos", JSON.stringify(todos)); // 다시 세팅
 }
